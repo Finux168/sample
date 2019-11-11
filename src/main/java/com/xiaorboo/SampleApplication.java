e2534e65eca7c1ea169c4bf8d6ec12cabde0dc6d
Finux <331665286@qq.com>
2019-11-11 22:31:13 +0800

    集成springboot便于分析源码

---
+++ src/main/java/com/xiaorboo/SampleApplication.java
@@ -0,0 +1,17 @@
+package com.xiaorboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @Author: fgd
+ * @Date: 2019-11-11
+ */
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class,args);
+    }
+}
